79cb6c382148cd848b3fb7d201c18fc0974d0150
ny-lain <151725597+ny-lain@users.noreply.github.com>
2025-05-30 16:02:41 +0700

    Update index.js

--- API-GATEWAY/index.js
+++ API-GATEWAY/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
-app.use('/Student', createProxyMiddleware({ target: 'http://localhost:5001', changeOrigin: true }));
-app.use('/Teacher', createProxyMiddleware({ target: 'http://localhost:5002', changeOrigin: true }));
+app.use('/Student', createProxyMiddleware({ target: 'http://44.203.155.164:5001', changeOrigin: true }));
+app.use('/Teacher', createProxyMiddleware({ target: 'http://52.201.218.98:5002', changeOrigin: true }));
 
 app.listen(5000, () => console.log('API Gateway running on port 5000'));
